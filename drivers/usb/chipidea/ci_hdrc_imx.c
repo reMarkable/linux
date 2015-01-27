37763a37a77150a4381a408c5da4ab06dffc0a07
Li Jun <jun.li@freescale.com>
2015-01-27 10:25:25 +0800

    MLK-10132-4 usb: chipidea: imx: disable usb wakeup in probe
    Disable usb wakeup as initial setting in probe.
    
    Signed-off-by: Li Jun <jun.li@freescale.com>
    (cherry picked from commit bf54f23766bcebd781f1c09bd68bc00d790160c8)

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -439,6 +439,16 @@ static int ci_hdrc_imx_probe(struct platform_device *pdev)
 		goto disable_device;
 	}
 
+	ret = imx_usbmisc_set_wakeup(data->usbmisc_data, false);
+	if (ret) {
+		dev_err(&pdev->dev, "usbmisc set_wakeup failed, ret=%d\n", ret);
+		goto disable_device;
+	}
+
+	/* usbmisc needs to know dr mode to choose wakeup setting */
+	data->usbmisc_data->available_role =
+			ci_hdrc_query_available_role(data->ci_pdev);
+
 	if (data->supports_runtime_pm) {
 		pm_runtime_set_active(dev);
 		pm_runtime_enable(dev);
