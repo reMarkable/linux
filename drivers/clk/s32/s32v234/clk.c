457bcf4d5d802c2b4076e58a18dabff31cce763c
Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>
2015-06-08 18:55:18 +0300

    clk: Add clk support for S32V234
    Add clock framework for Treerunner (S32V234), based on code from the i.MX
    3.10.17 codebase[1]. Add clock definitions that are used in the clocks
    vector (tree). At this point, the only PLL enabled is PERIPH-PLL.
    
    [1] https://source.codeaurora.org/external/imx/linux-imx/tree/?h=imx_3.10.17_1.0.0_ga_caf
    
    Signed-off-by: Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>
    Signed-off-by: Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>

---
+++ drivers/clk/s32/s32v234/clk.c
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright 2015-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017-2018 NXP
+ */
+
+#include <linux/of_address.h>
+#include <linux/clk.h>
+#include <dt-bindings/clock/s32v234-clock.h>
+
+#include "clk.h"
+
+static void __iomem *mc_cgm0_base;
+static void __iomem *mc_me_base;
+static void __iomem *src_base;
+
+DEFINE_SPINLOCK(s32v234_lock);
+
+/* sources for multiplexer clocks, this is used multiple times */
+PNAME(osc_sels) = {"firc", "fxosc", };
+
+static struct clk *clk[S32V234_CLK_END];
+static struct clk_onecell_data clk_data;
+
+static void __init s32v234_clocks_init(struct device_node *mc_cgm0_node)
+{
+	struct device_node *np;
+
+	clk[S32V234_CLK_DUMMY] = s32_clk_fixed("dummy", 0);
+	clk[S32V234_CLK_FXOSC] = s32_obtain_fixed_clock("fxosc", 0);
+	clk[S32V234_CLK_FIRC] = s32_obtain_fixed_clock("firc", 0);
+
+	np = of_find_compatible_node(NULL, NULL, "fsl,s32v234-mc_me");
+	mc_me_base = of_iomap(np, 0);
+	if (WARN_ON(!mc_me_base))
+		return;
+
+	np = of_find_compatible_node(NULL, NULL, "fsl,s32v234-src");
+	src_base = of_iomap(np, 0);
+	if (WARN_ON(!src_base))
+		return;
+
+	np = mc_cgm0_node;
+	mc_cgm0_base = of_iomap(np, 0);
+	if (WARN_ON(!mc_cgm0_base))
+		return;
+
+	enable_cpumodes_onperipheralconfig(mc_me_base, MC_ME_RUN_PCn_DRUN |
+					    MC_ME_RUN_PCn_RUN0 |
+					    MC_ME_RUN_PCn_RUN1 |
+					    MC_ME_RUN_PCn_RUN2 |
+					    MC_ME_RUN_PCn_RUN3,
+					    0);
+
+	/* turn on XOSC and FIRC */
+	enable_clocks_sources(MC_ME_MODE_MC_MVRON, MC_ME_MODE_MC_XOSCON |
+			      MC_ME_MODE_MC_FIRCON,
+			      MC_ME_RUNn_MC(mc_me_base, 0));
+
+	/* transition the core to RUN0 mode */
+	entry_to_target_mode(mc_me_base, MC_ME_MCTL_RUN0);
+
+	clk[S32V234_CLK_PERIPHPLL_SRC_SEL] = s32_clk_mux("periphpll_sel",
+		SRC_GPR1, SRC_GPR1_PERIPHPLL_SRC_SEL_OFFSET,
+		SRC_GPR1_XPLL_SRC_SEL_SIZE,
+		osc_sels, ARRAY_SIZE(osc_sels), &s32v234_lock);
+
+	/* PERIPH_PLL */
+	clk[S32V234_CLK_PERIPHPLL_VCO] = s32v234_clk_plldig(S32_PLLDIG_PERIPH,
+		"periphpll_vco", "periphpll_sel",
+		PERIPHPLL_PLLDIG(mc_cgm0_base),
+		PERIPHPLL_PLLDIG_PLLDV_MFD, PERIPHPLL_PLLDIG_PLLDV_MFN,
+		PERIPHPLL_PLLDIG_PLLDV_RFDPHI0,
+		PERIPHPLL_PLLDIG_PLLDV_RFDPHI1);
+
+	clk[S32V234_CLK_PERIPHPLL_PHI0] =
+		s32v234_clk_plldig_phi(S32_PLLDIG_PERIPH,
+		"periphpll_phi0", "periphpll_vco",
+		PERIPHPLL_PLLDIG(mc_cgm0_base), 0);
+
+	clk[S32V234_CLK_PERIPHPLL_PHI1] =
+		s32v234_clk_plldig_phi(S32_PLLDIG_PERIPH,
+		"periphpll_phi1", "periphpll_vco",
+		PERIPHPLL_PLLDIG(mc_cgm0_base), 1);
+
+	clk[S32V234_CLK_PERIPHPLL_PHI0_DIV3] = s32_clk_fixed_factor(
+		"periphpll_phi0_div3", "periphpll_phi0", 1, 3);
+
+	clk[S32V234_CLK_PERIPHPLL_PHI0_DIV5] = s32_clk_fixed_factor(
+		"periphpll_phi0_div5", "periphpll_phi0", 1, 5);
+
+	/* enable PERIPHPLL */
+	enable_clocks_sources(0, MC_ME_MODE_MC_PERIPHPLL,
+			      MC_ME_RUNn_MC(mc_me_base, 0));
+
+	/* set the system clock */
+	enable_sysclock(MC_ME_MODE_MC_SYSCLK(0x2),
+			MC_ME_RUNn_MC(mc_me_base, 0));
+
+	/* transition the core to RUN0 mode */
+	entry_to_target_mode(mc_me_base, MC_ME_MCTL_RUN0);
+
+	/* Add the clocks to provider list */
+	clk_data.clks = clk;
+	clk_data.clk_num = ARRAY_SIZE(clk);
+	of_clk_add_provider(np, of_clk_src_onecell_get, &clk_data);
+}
+
+CLK_OF_DECLARE(S32V234, "fsl,s32v234-mc_cgm0", s32v234_clocks_init);
