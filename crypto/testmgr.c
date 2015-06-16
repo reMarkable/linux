488997bccb2466dc422b3cf7ae8352c6342be6eb
Herbert Xu <herbert@gondor.apana.org.au>
2015-06-16 13:54:16 +0800

    crypto: testmgr - Disable rfc4543 test
    Because the old rfc4543 implementation always injected an IV into
    the AD, while the new one does not, we have to disable the test
    while it is converted over to the new AEAD interface.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -3733,7 +3733,7 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
-		.alg = "rfc4543(gcm(aes))",
+		.alg = "rfc4543(gcm(aes))-disabled",
 		.test = alg_test_aead,
 		.suite = {
 			.aead = {
