8097be0790bfe356b1c87b369c1db7cfca4f4c57
Martin T. H. Sandsmark <martin.sandsmark@remarkable.no>
2016-07-27 12:09:59 +0200

    Initial device tree

---
+++ arch/arm/boot/dts/zero-gravitas-padctl.h
@@ -0,0 +1,62 @@
+#ifndef ZERO_GRAVITAS_PADCTL_H
+#define ZERO_GRAVITAS_PADCTL_H
+
+#define PAD_CTL_NONE		0x80000000
+
+#define PAD_CTL_HYS		(1 << 16)
+
+#define PAD_CTL_PUS_100K_DOWN	(0 << 14 | PAD_CTL_PUE)
+#define PAD_CTL_PUS_47K_UP	(1 << 14 | PAD_CTL_PUE)
+#define PAD_CTL_PUS_100K_UP	(2 << 14 | PAD_CTL_PUE)
+#define PAD_CTL_PUS_22K_UP	(3 << 14 | PAD_CTL_PUE)
+#define PAD_CTL_PUE		(1 << 13 | PAD_CTL_PKE)
+#define PAD_CTL_PKE		(1 << 12)
+
+#define PAD_CTL_ODE		(1 << 11)
+
+#define PAD_CTL_SPEED_LOW	(1 << 6)
+#define PAD_CTL_SPEED_MED	(2 << 6)
+#define PAD_CTL_SPEED_HIGH	(3 << 6)
+
+#define PAD_CTL_DSE_DISABLE	(0 << 3)
+#define PAD_CTL_DSE_240ohm	(1 << 3)
+#define PAD_CTL_DSE_120ohm	(2 << 3)
+#define PAD_CTL_DSE_80ohm	(3 << 3)
+#define PAD_CTL_DSE_60ohm	(4 << 3)
+#define PAD_CTL_DSE_48ohm	(5 << 3)
+#define PAD_CTL_DSE_40ohm	(6 << 3)
+#define PAD_CTL_DSE_34ohm	(7 << 3)
+
+#define PAD_CTL_LVE		(1 << 1)
+#define PAD_CTL_LVE_BIT		(1 << 22)
+
+#define PAD_CTL_SRE_SLOW	(0 << 0)
+#define PAD_CTL_SRE_FAST	(1 << 0)
+
+/* USDHC CMD and DATA */    /* PAD_CTL_PUS_47K_UP? */
+#define PAD_CTL_USDHC_DEFAULT (PAD_CTL_PKE |	\
+	PAD_CTL_SPEED_LOW | PAD_CTL_DSE_48ohm |		\
+	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define PAD_CTL_USDHC_100MHZ (PAD_CTL_PKE |	\
+	PAD_CTL_SPEED_MED | PAD_CTL_DSE_48ohm |		\
+	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define PAD_CTL_USDHC_200MHZ (PAD_CTL_PKE |	\
+	PAD_CTL_SPEED_HIGH | PAD_CTL_DSE_48ohm |	\
+	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+/* USDHC CLK */
+#define PAD_CTL_USDHC_CLK_DEFAULT (PAD_CTL_PKE |	\
+	PAD_CTL_SPEED_LOW | PAD_CTL_DSE_48ohm |		\
+	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define PAD_CTL_USDHC_CLK_100MHZ (PAD_CTL_PKE |		\
+	PAD_CTL_SPEED_MED | PAD_CTL_DSE_48ohm |		\
+	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define PAD_CTL_USDHC_CLK_200MHZ (PAD_CTL_PKE |		\
+	PAD_CTL_SPEED_HIGH | PAD_CTL_DSE_48ohm |	\
+	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#endif//ZERO_GRAVITAS_PADCTL_H
