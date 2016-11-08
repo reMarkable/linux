9feebdbadd4fd9758a24bb8db0e4d36f20981122
Anson Huang <Anson.Huang@nxp.com>
2016-11-08 20:28:58 +0800

    MLK-13441-17 ARM: imx: add cpuidle support for i.mx7ulp
    Add i.MX7ULP cpuidle support, 3 levels idle as below:
    
    1. patial stop mode 3;
    2. patial stop mode 2;
    3. patial stop mode 1.
    
    PSTOP1 - Partial Stop with system and bus clock disabled
    PSTOP2 - Partial Stop with system clock disabled and bus clock enabled
    PSTOP3 - Partial Stop with system clock enabled and bus clock enabled
    
    All drivers has DMA function need to add pm_qos
    API to prevent cpuidle from entering PSTOP 1/2.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

--- arch/arm/mach-imx/cpuidle.h
+++ arch/arm/mach-imx/cpuidle.h
@@ -19,6 +19,7 @@ extern int imx6sx_cpuidle_init(void);
 extern int imx6ul_cpuidle_init(void);
 extern int imx7d_cpuidle_init(void);
 extern int imx7d_enable_rcosc(void);
+extern int imx7ulp_cpuidle_init(void);
 #else
 static inline int imx5_cpuidle_init(void)
 {
@@ -49,6 +50,10 @@ static inline int imx7d_cpuidle_init(void)
 	return 0;
 }
 static inline int imx7d_enable_rcosc(void)
+{
+	return 0;
+}
+static inline int imx7ulp_cpuidle_init(void)
 {
 	return 0;
 }
