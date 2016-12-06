a125eea07cdfca384fdef5e33fed842414347d2d
Peter Chen <peter.chen@nxp.com>
2016-12-06 10:06:31 +0800

    MLK-13570-5 usb: chipidea: imx: introduce pmqos for imx7ulp
    At imx7ulp, if the system enters idle, it will close some clocks and affect
    USB transfer. In order to avoid it, we request pmqos to avoid system
    entering idle when the USB is in use.
    
    Signed-off-by: Peter Chen <peter.chen@nxp.com>

--- include/linux/usb/chipidea.h
+++ include/linux/usb/chipidea.h
@@ -59,6 +59,8 @@ struct ci_hdrc_platform_data {
 #define CI_HDRC_OVERRIDE_RX_BURST	BIT(11)
 #define CI_HDRC_IMX_EHCI_QUIRK		BIT(12)
 #define CI_HDRC_IMX_IS_HSIC		BIT(13)
+/* need request pmqos during low power */
+#define CI_HDRC_PMQOS			BIT(14)
 	enum usb_dr_mode	dr_mode;
 #define CI_HDRC_CONTROLLER_RESET_EVENT		0
 #define CI_HDRC_CONTROLLER_STOPPED_EVENT	1
