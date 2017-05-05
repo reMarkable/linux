e5f0a89010c50a01b62bc61884738e5c241a9840
Daniel Baluta <daniel.baluta@nxp.com>
2017-05-05 10:09:55 +0300

    MLK-14528: ASoC: sdma: Update period/segment max bytes
    Commit 665ced16cf044 ("MLK-10050 dma: imx-sdma: add support for sdma
    memory copy") enforces maximum SDMA buffer descriptor length at 65532,
    but doesn't update period_bytes_max or max_segment size in DMA drivers.
    
    Thus, resulting in the following bug:
    
    $ arecord -Dhw:0,0 -r 192000 -f S20_3LE -c 1 -d 10 audio192k20b1c.wav
    imx-sdma: SDMA channel 5: maximum period size exceeded: 65534 > 65532
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- sound/soc/fsl/imx-pcm-dma.c
+++ sound/soc/fsl/imx-pcm-dma.c
@@ -42,7 +42,7 @@ static const struct snd_pcm_hardware imx_pcm_hardware = {
 		SNDRV_PCM_INFO_RESUME,
 	.buffer_bytes_max = IMX_DEFAULT_DMABUF_SIZE,
 	.period_bytes_min = 128,
-	.period_bytes_max = 65535, /* Limited by SDMA engine */
+	.period_bytes_max = 65532, /* Limited by SDMA engine */
 	.periods_min = 2,
 	.periods_max = 255,
 	.fifo_size = 0,
