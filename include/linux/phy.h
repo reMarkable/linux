6355c6bc23482c3345cc65634722470e24c1e41f
Bhaskar Upadhaya <Bhaskar.Upadhaya@nxp.com>
2017-11-29 15:27:57 +0530

    phy: Add 2.5G SGMII interface mode
    Add 2.5G SGMII interface mode(PHY_INTERFACE_MODE_2500SGMII)
    in existing phy_interface list
    
    Signed-off-by: Bhaskar Upadhaya <Bhaskar.Upadhaya@nxp.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -102,6 +102,7 @@ typedef enum {
 	/* 10GBASE-KR, XFI, SFI - single lane 10G Serdes */
 	PHY_INTERFACE_MODE_10GKR,
 	PHY_INTERFACE_MODE_USXGMII,
+	PHY_INTERFACE_MODE_2500SGMII,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -179,6 +180,8 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "10gbase-kr";
 	case PHY_INTERFACE_MODE_USXGMII:
 		return "usxgmii";
+	case PHY_INTERFACE_MODE_2500SGMII:
+		return "sgmii-2500";
 	default:
 		return "unknown";
 	}
