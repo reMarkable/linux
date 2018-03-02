bf8d6bbcc4d70d62cea86a01018ff8defae3f20e
Wright Feng <wright.feng@cypress.com>
2018-03-02 00:45:32 -0600

    MLK-18675-19 brcmfmac: make firmware eap_restrict a module parameter
    When eap_restrict is enabled, firmware will toss non-802.1x frames from
    tx/rx data path if station not yet authorized.
    Internal firmware eap_restrict is disabled by default. This patch makes
    it possible to enable firmware eap_restrict by specifying
    eap_restrict=1 as module parameter.
    
    Signed-off-by: Wright Feng <wright.feng@cypress.com>
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    (Vipul: Fixed merge conflicts)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
@@ -67,6 +67,10 @@ static int brcmf_iapp_enable;
 module_param_named(iapp, brcmf_iapp_enable, int, 0);
 MODULE_PARM_DESC(iapp, "Enable partial support for the obsoleted Inter-Access Point Protocol");
 
+static int brcmf_eap_restrict;
+module_param_named(eap_restrict, brcmf_eap_restrict, int, 0400);
+MODULE_PARM_DESC(eap_restrict, "Block non-802.1X frames until auth finished");
+
 static int brcmf_sdio_dpc_prio;
 module_param_named(sdio_dpc_prio, brcmf_sdio_dpc_prio, int, S_IRUSR);
 MODULE_PARM_DESC(sdio_dpc_prio, "The scheduling priority of sdio_dpc thread");
@@ -417,6 +421,7 @@ struct brcmf_mp_device *brcmf_get_module_param(struct device *dev,
 	settings->fcmode = brcmf_fcmode;
 	settings->roamoff = !!brcmf_roamoff;
 	settings->iapp = !!brcmf_iapp_enable;
+	settings->eap_restrict = !!brcmf_eap_restrict;
 	settings->sdio_dpc_prio = brcmf_sdio_dpc_prio;
 #ifdef DEBUG
 	settings->ignore_probe_fail = !!brcmf_ignore_probe_fail;
