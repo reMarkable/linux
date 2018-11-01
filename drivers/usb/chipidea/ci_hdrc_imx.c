a320ed8dd72a9c4be6546fd9c21ba290b7450409
Peter Chen <peter.chen@nxp.com>
2018-11-01 14:47:20 +0800

    MLK-20158-1 usb: chipidea: ci_hdrc_imx: restore pinctrl
    The pinctrl setting may lost during the system suspend
    (eg, imx7ulp), we need to restore it after system resume.
    Meanwhile, some platforms may need to set special pinctrl
    for power comsumption.
    
    Signed-off-by: Peter Chen <peter.chen@nxp.com>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -630,7 +630,12 @@ static int __maybe_unused ci_hdrc_imx_suspend(struct device *dev)
 		}
 	}
 
-	return imx_controller_suspend(dev);
+	ret = imx_controller_suspend(dev);
+	if (ret)
+		return ret;
+
+	pinctrl_pm_select_sleep_state(dev);
+	return ret;
 }
 
 static int __maybe_unused ci_hdrc_imx_resume(struct device *dev)
@@ -638,6 +643,7 @@ static int __maybe_unused ci_hdrc_imx_resume(struct device *dev)
 	struct ci_hdrc_imx_data *data = dev_get_drvdata(dev);
 	int ret;
 
+	pinctrl_pm_select_default_state(dev);
 	ret = imx_controller_resume(dev);
 	if (!ret && data->supports_runtime_pm) {
 		pm_runtime_disable(dev);
