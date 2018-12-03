00175c45671934f4831c7f9753704cccfe5b9f3a
Wright Feng <wright.feng@cypress.com>
2018-12-03 17:39:33 +0800

    MLK-20679 brcmfmac: reset two D11 cores if chip has two D11 cores
    There are two D11 cores in RSDB chips like 4359. We have to reset two
    D11 cores simutaneously before firmware download, or the firmware may
    not be initialized correctly and cause "fw initialized failed" error.
    
    Reviewed-by: Richard Zhu <hongxing.zhu@nxp.com>
    Signed-off-by: Wright Feng <wright.feng@cypress.com>
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    (cherry picked commit from 8f50bea9357af3088dba0103f0017d56763b0ae9)

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/chip.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/chip.h
@@ -76,6 +76,7 @@ void brcmf_chip_detach(struct brcmf_chip *chip);
 struct brcmf_core *brcmf_chip_get_core(struct brcmf_chip *chip, u16 coreid);
 struct brcmf_core *brcmf_chip_get_chipcommon(struct brcmf_chip *chip);
 struct brcmf_core *brcmf_chip_get_pmu(struct brcmf_chip *pub);
+struct brcmf_core *brcmf_chip_get_d11core(struct brcmf_chip *pub, u8 unit);
 bool brcmf_chip_iscoreup(struct brcmf_core *core);
 void brcmf_chip_coredisable(struct brcmf_core *core, u32 prereset, u32 reset);
 void brcmf_chip_resetcore(struct brcmf_core *core, u32 prereset, u32 reset,
