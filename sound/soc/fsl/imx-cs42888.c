1492f5267f3c0f11381c705d2f8151fe769cc0fa
Shengjiu Wang <shengjiu.wang@nxp.com>
2019-01-29 13:10:48 +0800

    ASoC: imx-cs42888: Use -EPROBE_DEFER instead of -EINVAL
    Use -EPROBE_DEFER instead of -EINVAL for the resource is
    not ready in time
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- sound/soc/fsl/imx-cs42888.c
+++ sound/soc/fsl/imx-cs42888.c
@@ -373,7 +373,7 @@ static int imx_cs42888_probe(struct platform_device *pdev)
 		codec_dev = of_find_i2c_device_by_node(codec_np);
 		if (!codec_dev || !codec_dev->dev.driver) {
 			dev_err(&pdev->dev, "failed to find codec platform device\n");
-			ret = -EINVAL;
+			ret = -EPROBE_DEFER;
 			goto fail;
 		}
 
