2f5eda5940636155ab1e0ce5d7cd563bf963c8e1
Ioana Radulescu <ruxandra.radulescu@nxp.com>
2019-03-13 19:12:47 +0200

    staging: fsl-dpaa2/mac: add pause frames support for managed phys
    Read the configured dpmac options and depending on the supported
    features set the according advertising bit.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>
    Signed-off-by: Bogdan Purcareata <bogdan.purcareata@nxp.com>
    Signed-off-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>

--- drivers/staging/fsl-dpaa2/mac/mac.c
+++ drivers/staging/fsl-dpaa2/mac/mac.c
@@ -148,6 +148,11 @@ static void dpaa2_mac_link_changed(struct net_device *netdev)
 		if (phydev->autoneg)
 			state.options |= DPMAC_LINK_OPT_AUTONEG;
 
+		if (phydev->pause && linkmode_test_bit(ETHTOOL_LINK_MODE_Pause_BIT, phydev->advertising))
+			state.options |= DPMAC_LINK_OPT_PAUSE;
+		if (phydev->pause && linkmode_test_bit(ETHTOOL_LINK_MODE_Asym_Pause_BIT, phydev->advertising))
+			state.options |= DPMAC_LINK_OPT_ASYM_PAUSE;
+
 		netif_carrier_on(netdev);
 	} else {
 		netif_carrier_off(netdev);
@@ -413,6 +418,20 @@ static void configure_link(struct dpaa2_mac_priv *priv,
 	if (cfg->advertising != 0) {
 		linkmode_zero(phydev->advertising);
 		link_mode_dpmac2phydev(cfg->advertising, phydev->advertising);
+	}
+
+	if (linkmode_test_bit(ETHTOOL_LINK_MODE_Pause_BIT, phydev->supported)) {
+		if (cfg->options & DPMAC_LINK_OPT_PAUSE)
+			linkmode_set_bit(ETHTOOL_LINK_MODE_Pause_BIT, phydev->advertising);
+		else
+			linkmode_clear_bit(ETHTOOL_LINK_MODE_Pause_BIT, phydev->advertising);
+	}
+
+	if (linkmode_test_bit(ETHTOOL_LINK_MODE_Asym_Pause_BIT, phydev->supported)) {
+		if (cfg->options & DPMAC_LINK_OPT_ASYM_PAUSE)
+			linkmode_set_bit(ETHTOOL_LINK_MODE_Asym_Pause_BIT, phydev->advertising);
+		else
+			linkmode_clear_bit(ETHTOOL_LINK_MODE_Asym_Pause_BIT, phydev->advertising);
 	}
 
 	if (cfg->options & DPMAC_LINK_OPT_AUTONEG) {
