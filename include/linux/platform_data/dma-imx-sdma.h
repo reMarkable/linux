2d1e5b8f23c20ccdd2336685467f72e618715658
Robin Gong <yibin.gong@nxp.com>
2019-04-24 14:19:58 +0800

    MLK-21309: dma: engine: sdma: add uart rom script
    For syncing with unstreaming kernel on UART driver from 4.19 changed
    to rom script for uart rx path, and the compatiblity of legacy kernel
    using ram script, add both uart rom and ram script support, so add
    rom script address.
    
    ram script:
            uart_2_mcu_fix_addr
            uartsh_2_mcu_fix_addr /* through spba bus */
    rom script:
            uart_2_mcu_addr
            uartsh_2_mcu_addr /* through spba bus */
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>

--- include/linux/platform_data/dma-imx-sdma.h
+++ include/linux/platform_data/dma-imx-sdma.h
@@ -20,12 +20,12 @@ struct sdma_script_start_addrs {
 	s32 per_2_firi_addr;
 	s32 mcu_2_firi_addr;
 	s32 uart_2_per_addr;
-	s32 uart_2_mcu_addr;
+	s32 uart_2_mcu_ram_addr;
 	s32 per_2_app_addr;
 	s32 mcu_2_app_addr;
 	s32 per_2_per_addr;
 	s32 uartsh_2_per_addr;
-	s32 uartsh_2_mcu_addr;
+	s32 uartsh_2_mcu_ram_addr;
 	s32 per_2_shp_addr;
 	s32 mcu_2_shp_addr;
 	s32 ata_2_mcu_addr;
@@ -54,6 +54,8 @@ struct sdma_script_start_addrs {
 	s32 mcu_2_ecspi_addr;
 	s32 mcu_2_sai_addr;
 	s32 sai_2_mcu_addr;
+	s32 uart_2_mcu_addr;
+	s32 uartsh_2_mcu_addr;
 	/* End of v3 array */
 	s32 mcu_2_zqspi_addr;
 	/* End of v4 array */
