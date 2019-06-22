297bfc6c1d1f6bd66049636e98ae5986b6d9f641
Joakim Zhang <qiangqing.zhang@nxp.com>
2019-06-22 15:07:41 +0800

    MLK-22216 mxc: emvsim: last character need be transmitted separately with no guard time
    It can't receive when transmitting on EMVSIM IP. ICC will send response
    quickly when Terminal transmits last character with guard time. If the
    response came to Terminal before the guard time expired, the Terminal
    would not receive the respone. So should transmit last character with no
    guard time.
    
    This patch intends to fix the communication failure when setting guard
    time.
    
    This will cause about 1 ETU delay before transmitting last character
    (according to timing measured), but it does not matter.
    
    Signed-off-by: Joakim Zhang <qiangqing.zhang@nxp.com>
    Acked-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/mxc/sim/imx_emvsim.c
+++ drivers/mxc/sim/imx_emvsim.c
@@ -197,6 +197,7 @@ struct emvsim_t {
 	spinlock_t lock;
 	u32 clk_rate;
 	u8 checking_ts_timing;
+	u8 tx_last_character;
 };
 
 static struct miscdevice emvsim_dev;
@@ -444,23 +445,58 @@ static int32_t emvsim_check_rec_data(u32 *reg_data)
 	return err;
 }
 
+static void emvsim_enable_guardtime(struct emvsim_t *emvsim, int enable)
+{
+	/* transmitter: set Guard Time Value in ETU */
+	if (enable) {
+		if (emvsim->protocol_type == SIM_PROTOCOL_T0) {
+			/*
+			 * From EMV4.3, TotalETU = 12 + CGT.
+			 * If cgt equals 0xFF, TotalETU = 12.
+			 */
+			if (emvsim->timing_data.cgt == 0xFF)
+				__raw_writel(0, emvsim->ioaddr + EMV_SIM_TX_GETU);
+			else
+				__raw_writel(emvsim->timing_data.cgt,
+					     emvsim->ioaddr + EMV_SIM_TX_GETU);
+		} else if (emvsim->protocol_type == SIM_PROTOCOL_T1) {
+			/*
+			 * From EMV4.3, TotalETU = 12 + CGT.
+			 * If cgt equals 0xFF, TotalETU = 11.
+			 */
+			__raw_writel(emvsim->timing_data.cgt,
+				     emvsim->ioaddr + EMV_SIM_TX_GETU);
+		}
+	} else {
+		__raw_writel(0, emvsim->ioaddr + EMV_SIM_TX_GETU);
+	}
+}
+
 static void emvsim_xmt_fill_fifo(struct emvsim_t *emvsim)
 {
 	u32 reg_data;
 	u32 bytesleft, i;
 
-	reg_data = __raw_readl(emvsim->ioaddr + EMV_SIM_TX_STATUS);
-	bytesleft = SIM_TX_FIFO_DEPTH - ((reg_data >> 24) & 0x1F);
+	if (!emvsim->tx_last_character) {
+		reg_data = __raw_readl(emvsim->ioaddr + EMV_SIM_TX_STATUS);
+		bytesleft = SIM_TX_FIFO_DEPTH - ((reg_data >> 24) & 0x1F);
 
-	if (bytesleft > emvsim->xmt_remaining)
-		bytesleft = emvsim->xmt_remaining;
+		if (bytesleft > emvsim->xmt_remaining)
+			bytesleft = emvsim->xmt_remaining;
 
-	for (i = 0; i < bytesleft; i++) {
+		for (i = 0; i < bytesleft; i++) {
+			__raw_writel(emvsim->xmt_buffer[emvsim->xmt_pos],
+				     emvsim->ioaddr + EMV_SIM_TX_BUF);
+			emvsim->xmt_pos++;
+		};
+		emvsim->xmt_remaining -= bytesleft;
+	} else {
+		/* clear guard time before sending last character */
+		emvsim_enable_guardtime(emvsim, 0);
+
 		__raw_writel(emvsim->xmt_buffer[emvsim->xmt_pos],
 			     emvsim->ioaddr + EMV_SIM_TX_BUF);
-		emvsim->xmt_pos++;
-	};
-	emvsim->xmt_remaining -= bytesleft;
+	}
 };
 
 static void emvsim_rcv_read_fifo(struct emvsim_t *emvsim)
@@ -678,7 +714,12 @@ static irqreturn_t emvsim_irq_handler(int irq, void *dev_id)
 				__raw_writel(reg_data, emvsim->ioaddr +
 					     EMV_SIM_INT_MASK);
 			}
-		} else if ((tx_status & TCF) && !emvsim->xmt_remaining) {
+		} else if ((tx_status & TCF) && !emvsim->xmt_remaining &&
+			   !emvsim->tx_last_character) {
+			emvsim->tx_last_character = 1;
+			emvsim_xmt_fill_fifo(emvsim);
+		} else if ((tx_status & TCF) && !emvsim->xmt_remaining &&
+			   emvsim->tx_last_character) {
 			emvsim_tx_irq_disable(emvsim);
 			emvsim_set_rx(emvsim, 1);
 			emvsim->state = SIM_STATE_XMT_DONE;
@@ -973,29 +1014,15 @@ static void emvsim_set_timer_counter(struct emvsim_t *emvsim)
 		__raw_writel(emvsim->timing_data.bwt,
 			     emvsim->ioaddr + EMV_SIM_BWT_VAL);
 
-	/* 11 etu and 12 etu, T0: 12ETU; T1: 11ETU */
+	/* receiver: 12 etu and 11 etu, T0: 12ETU; T1: 11ETU */
 	if (emvsim->protocol_type == SIM_PROTOCOL_T0) {
-		/*
-		 * From EMV4.3 , T0 mode means 12 ETU. TotalETU=12+CGT.
-		 * If cgt equals 0xFF, TotalETU = 12
-		 */
 		reg = __raw_readl(emvsim->ioaddr + EMV_SIM_CTRL);
 		reg &= ~RCVR_11;
 		 __raw_writel(reg, emvsim->ioaddr + EMV_SIM_CTRL);
-
-		/* set Transmitter Guard Time Value in ETU */
-		if (emvsim->timing_data.cgt == 0xFF)
-			__raw_writel(0, emvsim->ioaddr + EMV_SIM_TX_GETU);
-		else
-			__raw_writel(emvsim->timing_data.cgt,
-				     emvsim->ioaddr + EMV_SIM_TX_GETU);
 	} else if (emvsim->protocol_type == SIM_PROTOCOL_T1) {
-		/* From EMV4.3 , T1 mode means 11 ETU. TotalETU=11+CGT */
 		reg = __raw_readl(emvsim->ioaddr + EMV_SIM_CTRL);
 		reg |= RCVR_11;
 		__raw_writel(reg, emvsim->ioaddr + EMV_SIM_CTRL);
-		__raw_writel(emvsim->timing_data.cgt,
-			     emvsim->ioaddr + EMV_SIM_TX_GETU);
 	}
 }
 
@@ -1014,6 +1041,7 @@ static int emvsim_xmt_start(struct emvsim_t *emvsim)
 	}
 
 	emvsim_set_timer_counter(emvsim);
+	emvsim_enable_guardtime(emvsim, 1);
 
 	if (emvsim->xmt_remaining != 0) {
 		reg_val = __raw_readl(emvsim->ioaddr + EMV_SIM_TX_THD);
@@ -1055,6 +1083,7 @@ static void emvsim_start_rcv(struct emvsim_t *emvsim)
 	emvsim_set_rx(emvsim, 1);
 	emvsim_set_baud_rate(emvsim);
 	emvsim_set_timer_counter(emvsim);
+	emvsim_enable_guardtime(emvsim, 0);
 	emvsim_set_cwt(emvsim, 1);
 	emvsim_set_bwt(emvsim, 1);
 
@@ -1229,6 +1258,15 @@ static long emvsim_ioctl(struct file *file,
 			errval = ret;
 			break;
 		}
+
+		/* last character must be transmitted separately due to it
+		 * can't has guard time.
+		 */
+		emvsim->xmt_remaining -= 1;
+		if (!emvsim->xmt_remaining)
+			emvsim->tx_last_character = 1;
+		else
+			emvsim->tx_last_character = 0;
 
 		emvsim_clear_rx_buf(emvsim);
 		emvsim_set_cwt(emvsim, 0);
