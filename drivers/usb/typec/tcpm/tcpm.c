651f8d74dc25b04f75d4cac60194ec0defee162d
Li Jun <jun.li@nxp.com>
2019-08-19 10:32:53 +0800

    MLK-16013-17 usb: typec: tcpm: set cc when attached
    As the cc setting while drp toggling may not be the correct value
    for attached state, which only means the start value in case of
    tcpci, so update the cc setting when src/snk attached state.
    
    Signed-off-by: Li Jun <jun.li@nxp.com>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -2623,6 +2623,8 @@ static int tcpm_src_attach(struct tcpm_port *port)
 	if (port->attached)
 		return 0;
 
+	tcpm_set_cc(port, tcpm_rp_cc(port));
+
 	ret = tcpm_set_polarity(port, polarity);
 	if (ret < 0)
 		return ret;
@@ -2743,6 +2745,8 @@ static int tcpm_snk_attach(struct tcpm_port *port)
 
 	if (port->attached)
 		return 0;
+
+	tcpm_set_cc(port, TYPEC_CC_RD);
 
 	ret = tcpm_set_polarity(port, port->cc2 != TYPEC_CC_OPEN ?
 				TYPEC_POLARITY_CC2 : TYPEC_POLARITY_CC1);
