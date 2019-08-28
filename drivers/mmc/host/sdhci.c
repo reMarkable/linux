89a819705b4c4e8bc09f1ff85159a1d8df037299
Haibo Chen <haibo.chen@nxp.com>
2019-08-28 18:40:36 +0800

    mmc: sdhci: disable irq in sdhci host suspend ranther than free this irq
    Currently sdhci driver free irq in host suspend, and call
    request_threaded_irq() in host resume. But during host resume,
    Ctrl+C can impact sdhci host resume, see the error log:
    
    CPU1 is up
    PM: noirq resume of devices complete after 0.637 msecs imx-sdma 30bd0000.sdma: loaded firmware 4.1
    PM: early resume of devices complete after 0.774 msecs
    dpm_run_callback(): platform_pm_resume+0x0/0x44 returns -4
    PM: Device 30b40000.usdhc failed to resume: error -4
    dpm_run_callback(): platform_pm_resume+0x0/0x44 returns -4
    PM: Device 30b50000.usdhc failed to resume: error -4
    dpm_run_callback(): platform_pm_resume+0x0/0x44 returns -4
    PM: Device 30b60000.usdhc failed to resume: error -4 fec 30be0000.ethernet eth0: Link is Up - 100Mbps/Full - flow control rx/tx
    mmc0: Timeout waiting for hardware interrupt.
    mmc0: Timeout waiting for hardware interrupt.
    mmc0: Timeout waiting for hardware interrupt.
    mmc0: Timeout waiting for hardware interrupt.
    mmc0: Timeout waiting for hardware interrupt.
    mmc0: Timeout waiting for hardware interrupt.
    mmc0: error -110 during resume (card was removed?)
    mmc2: Timeout waiting for hardware interrupt.
    mmc2: Timeout waiting for hardware interrupt.
    mmc2: error -110 during resume (card was removed?)
    
    In request_threaded_irq-> __setup_irq-> kthread_create
    ->kthread_create_on_node, the comment shows that SIGKILLed will
    impact the kthread create, and return -EINTR.
    
    This patch replace them with disable|enable_irq(), that will prevent
    IRQs from being propagated to the sdhci driver.
    
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -3293,7 +3293,7 @@ int sdhci_suspend_host(struct sdhci_host *host)
 		host->ier = 0;
 		sdhci_writel(host, 0, SDHCI_INT_ENABLE);
 		sdhci_writel(host, 0, SDHCI_SIGNAL_ENABLE);
-		free_irq(host->irq, host);
+		disable_irq(host->irq);
 	}
 
 	return 0;
@@ -3304,7 +3304,6 @@ EXPORT_SYMBOL_GPL(sdhci_suspend_host);
 int sdhci_resume_host(struct sdhci_host *host)
 {
 	struct mmc_host *mmc = host->mmc;
-	int ret = 0;
 
 	if (host->flags & (SDHCI_USE_SDMA | SDHCI_USE_ADMA)) {
 		if (host->ops->enable_dma)
@@ -3325,16 +3324,12 @@ int sdhci_resume_host(struct sdhci_host *host)
 	if (host->irq_wake_enabled) {
 		sdhci_disable_irq_wakeups(host);
 	} else {
-		ret = request_threaded_irq(host->irq, sdhci_irq,
-					   sdhci_thread_irq, IRQF_SHARED,
-					   mmc_hostname(host->mmc), host);
-		if (ret)
-			return ret;
+		enable_irq(host->irq);
 	}
 
 	sdhci_enable_card_detection(host);
 
-	return ret;
+	return 0;
 }
 
 EXPORT_SYMBOL_GPL(sdhci_resume_host);
