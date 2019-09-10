77816ac1faa41fac82b75e66c4cbc4205c0926a1
Jun Li <jun.li@nxp.com>
2019-09-10 14:54:57 +0800

    usb: chipidea: udc: protect usb interrupt enable
    We hit the problem with below sequence:
    - ci_udc_vbus_session() update vbus_active flag and ci->driver
    is valid,
    - before calling the ci_hdrc_gadget_connect(),
    usb_gadget_udc_stop() is called by application remove gadget
    driver,
    - ci_udc_vbus_session() will contine do ci_hdrc_gadget_connect() as
    gadget_ready is 1, so udc interrupt is enabled, but ci->driver is
    NULL.
    - USB connection irq generated but ci->driver is NULL.
    
    As udc irq only should be enabled when gadget driver is binded, so
    add spinlock to protect the usb irq enable for vbus session handling.
    
    Signed-off-by: Jun Li <jun.li@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    (cherry picked from commit 72dc8df7920fc24eba0f586c56e900a1643ff2b3)
    (cherry picked from commit 0fe900249814f73ecb79c6fb2ae75d46ed9d3a3e)

--- drivers/usb/chipidea/udc.c
+++ drivers/usb/chipidea/udc.c
@@ -1632,12 +1632,9 @@ static int ci_udc_vbus_session(struct usb_gadget *_gadget, int is_active)
 {
 	struct ci_hdrc *ci = container_of(_gadget, struct ci_hdrc, gadget);
 	unsigned long flags;
-	int gadget_ready = 0;
 
 	spin_lock_irqsave(&ci->lock, flags);
 	ci->vbus_active = is_active;
-	if (ci->driver)
-		gadget_ready = 1;
 	spin_unlock_irqrestore(&ci->lock, flags);
 
 	if (ci->usb_phy) {
@@ -1650,7 +1647,7 @@ static int ci_udc_vbus_session(struct usb_gadget *_gadget, int is_active)
 			usb_phy_set_event(ci->usb_phy, USB_EVENT_NONE);
 	}
 
-	if (gadget_ready)
+	if (ci->driver)
 		ci_hdrc_gadget_connect(_gadget, is_active);
 
 	return 0;
@@ -1912,6 +1909,7 @@ static int ci_udc_stop(struct usb_gadget *gadget)
 	unsigned long flags;
 
 	spin_lock_irqsave(&ci->lock, flags);
+	ci->driver = NULL;
 
 	if (ci->vbus_active) {
 		hw_device_state(ci, 0);
@@ -1924,7 +1922,6 @@ static int ci_udc_stop(struct usb_gadget *gadget)
 		pm_runtime_put(ci->dev);
 	}
 
-	ci->driver = NULL;
 	spin_unlock_irqrestore(&ci->lock, flags);
 
 	ci_udc_stop_for_otg_fsm(ci);
@@ -2111,13 +2108,18 @@ int ci_usb_charger_connect(struct ci_hdrc *ci, int is_active)
 void ci_hdrc_gadget_connect(struct usb_gadget *gadget, int is_active)
 {
 	struct ci_hdrc *ci = container_of(gadget, struct ci_hdrc, gadget);
+	unsigned long flags;
 
 	if (is_active) {
 		pm_runtime_get_sync(ci->dev);
 		hw_device_reset(ci);
-		hw_device_state(ci, ci->ep0out->qh.dma);
-		usb_gadget_set_state(gadget, USB_STATE_POWERED);
-		usb_udc_vbus_handler(gadget, true);
+		spin_lock_irqsave(&ci->lock, flags);
+		if (ci->driver) {
+			hw_device_state(ci, ci->ep0out->qh.dma);
+			usb_gadget_set_state(gadget, USB_STATE_POWERED);
+			usb_udc_vbus_handler(gadget, true);
+		}
+		spin_unlock_irqrestore(&ci->lock, flags);
 	} else {
 		usb_udc_vbus_handler(gadget, false);
 		if (ci->driver)
