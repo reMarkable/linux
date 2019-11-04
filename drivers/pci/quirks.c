9c86847fadf16dea712674459403e079582f23ca
Fugang Duan <fugang.duan@nxp.com>
2019-11-04 13:48:52 +0800

    PCI: Disable MSI on marvel 88w9098 and 88w8997 chips
    i.MX8x with MSI enable suspend/resume doesn't work for
    marvell 88w9098 and 88w8997 wlan chips, disable the feature
    before the issue fixed.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -2454,6 +2454,9 @@ DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_VIA, PCI_DEVICE_ID_VIA_8380_0, quirk_disab
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_SI, 0x0761, quirk_disable_all_msi);
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_BROADCOM, 0x43ec, quirk_disable_all_msi);
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_BROADCOM, 0x43ef, quirk_disable_all_msi);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_MARVELL_EXT, 0x2b42, quirk_disable_all_msi);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_MARVELL_EXT, 0x2b43, quirk_disable_all_msi);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_MARVELL_EXT, 0x2b44, quirk_disable_all_msi);
 
 /* Disable MSI on chipsets that are known to not support it */
 static void quirk_disable_msi(struct pci_dev *dev)
