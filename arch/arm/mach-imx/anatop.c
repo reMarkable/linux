090b431c46bb79bcbc6314fee90e339e89552c82
Jacky Bai <ping.bai@nxp.com>
2019-11-27 14:47:28 +0800

    LF-171 ARM: imx: Add cpu type check for imx6ulz in msl code
    Add cpu type check for i.MX6ULZ in MSL code to support low
    power feature.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Anson Huang <Anson.Huang@nxp.com>

--- arch/arm/mach-imx/anatop.c
+++ arch/arm/mach-imx/anatop.c
@@ -63,8 +63,8 @@ static void imx_anatop_enable_weak2p5(bool enable)
 
 	regmap_read(anatop, ANADIG_ANA_MISC0, &val);
 
-	if (cpu_is_imx6sx() || cpu_is_imx6ul() || cpu_is_imx6ull()
-		|| cpu_is_imx6sll())
+	if (cpu_is_imx6sx() || cpu_is_imx6ul() || cpu_is_imx6ull() ||
+	    cpu_is_imx6ulz() || cpu_is_imx6sll())
 		mask = BM_ANADIG_ANA_MISC0_V3_STOP_MODE_CONFIG;
 	else if (cpu_is_imx6sl())
 		mask = BM_ANADIG_ANA_MISC0_V2_STOP_MODE_CONFIG;
@@ -92,7 +92,7 @@ static inline void imx_anatop_enable_2p5_pulldown(bool enable)
 static inline void imx_anatop_disconnect_high_snvs(bool enable)
 {
 	if (cpu_is_imx6sx() || cpu_is_imx6ul() || cpu_is_imx6ull() ||
-		cpu_is_imx6sll())
+	    cpu_is_imx6ulz() || cpu_is_imx6sll())
 		regmap_write(anatop, ANADIG_ANA_MISC0 +
 			(enable ? REG_SET : REG_CLR),
 			BM_ANADIG_ANA_MISC0_V2_DISCON_HIGH_SNVS);
@@ -147,7 +147,7 @@ void imx_anatop_pre_suspend(void)
 	imx_anatop_enable_fet_odrive(true);
 
 	if (cpu_is_imx6sl() || cpu_is_imx6sx() || cpu_is_imx6ul() ||
-		cpu_is_imx6ull() || cpu_is_imx6sll())
+	    cpu_is_imx6ull() || cpu_is_imx6ulz() || cpu_is_imx6sll())
 		imx_anatop_disconnect_high_snvs(true);
 }
 
@@ -172,7 +172,7 @@ void imx_anatop_post_resume(void)
 	imx_anatop_enable_fet_odrive(false);
 
 	if (cpu_is_imx6sl() || cpu_is_imx6sx() || cpu_is_imx6ul() ||
-		cpu_is_imx6ull() || cpu_is_imx6sll())
+	    cpu_is_imx6ull() || cpu_is_imx6ulz() || cpu_is_imx6sll())
 		imx_anatop_disconnect_high_snvs(false);
 }
 
