9c3a087b1db3807400723f5c6e9a13e4429cfc3f
Daniel Baluta <daniel.baluta@nxp.com>
2019-11-29 00:41:46 +0200

    ASoC: SOF: Read tplg filename from board descriptor
    We use the nocodec tplg_filename for now. Proper name
    should be read from device tree file.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- sound/soc/sof/sof-of-dev.c
+++ sound/soc/sof/sof-of-dev.c
@@ -93,6 +93,8 @@ static int sof_of_probe(struct platform_device *pdev)
 	sof_pdata->tplg_filename_prefix = sof_pdata->desc->default_tplg_path;
 
 	sof_pdata->fw_filename =  desc->default_fw_filename;
+	/* FIXME: Add proper value for tplg_filename */
+	sof_pdata->tplg_filename =  desc->nocodec_tplg_filename;
 
 #if IS_ENABLED(CONFIG_SND_SOC_SOF_PROBE_WORK_QUEUE)
 	/* set callback to enable runtime_pm */
