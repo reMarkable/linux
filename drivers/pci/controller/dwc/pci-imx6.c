d1fcbc345b82cd86e6c288b984ce2da0004eaf1d
Richard Zhu <hongxing.zhu@nxp.com>
2019-11-28 16:44:07 +0800

    LF-128 PCI: imx: turn off the clocks and regulators when link is down
    To save power consumption, disable pcie clocks and regulators when
    pcie link is down.
    
    Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>
    Reviewed-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -1472,6 +1472,14 @@ err_reset_phy:
 		dw_pcie_readl_dbi(pci, PCIE_PORT_DEBUG0),
 		dw_pcie_readl_dbi(pci, PCIE_PORT_DEBUG1));
 	imx6_pcie_reset_phy(imx6_pcie);
+	if (!IS_ENABLED(CONFIG_PCI_IMX6_COMPLIANCE_TEST)) {
+		imx6_pcie_clk_disable(imx6_pcie);
+		if (imx6_pcie->vpcie != NULL)
+			regulator_disable(imx6_pcie->vpcie);
+		if (imx6_pcie->epdev_on != NULL)
+			regulator_disable(imx6_pcie->epdev_on);
+	}
+
 	return ret;
 }
 
@@ -1504,7 +1512,8 @@ static int imx6_pcie_host_init(struct pcie_port *pp)
 	if (!IS_ENABLED(CONFIG_EP_MODE_IN_EP_RC_SYS)) {
 		dw_pcie_setup_rc(pp);
 		pci_imx_set_msi_en(pp);
-		imx6_pcie_establish_link(imx6_pcie);
+		if (imx6_pcie_establish_link(imx6_pcie))
+			return -ENODEV;
 
 		if (IS_ENABLED(CONFIG_PCI_MSI))
 			dw_pcie_msi_init(pp);
