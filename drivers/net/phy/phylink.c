57fe15baac62dad00817e3359cefc123e24f10a2
Vladimir Oltean <vladimir.oltean@nxp.com>
2019-11-28 03:21:53 +0200

    net: phylink: call mac_an_restart for SGMII/QSGMII inband interfaces too
    It doesn't quite make sense why restarting the AN process should be
    unique to 802.3z (1000Base-X) modes. It is valid to put an SGMII PCS in
    in-band AN mode, therefore also make PHYLINK re-trigger an
    auto-negotiation if needed.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -356,7 +356,9 @@ static void phylink_mac_config_up(struct phylink *pl,
 static void phylink_mac_an_restart(struct phylink *pl)
 {
 	if (pl->link_config.an_enabled &&
-	    phy_interface_mode_is_8023z(pl->link_config.interface))
+	    (phy_interface_mode_is_8023z(pl->link_config.interface) ||
+	     pl->link_config.interface == PHY_INTERFACE_MODE_SGMII ||
+	     pl->link_config.interface == PHY_INTERFACE_MODE_QSGMII))
 		pl->ops->mac_an_restart(pl->config);
 }
 
