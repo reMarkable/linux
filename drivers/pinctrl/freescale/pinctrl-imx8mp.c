ed45b2526a456e8f2d8ba1d3aa538ea134f3250f
Anson Huang <Anson.Huang@nxp.com>
2019-12-26 16:20:06 +0800

    MLK-23167-2 pinctrl: imx8mp: Skip unnecessary of_match_device operation
    Skip unnecessary of_match_device operation to simply code.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- drivers/pinctrl/freescale/pinctrl-imx8mp.c
+++ drivers/pinctrl/freescale/pinctrl-imx8mp.c
@@ -328,17 +328,7 @@ static struct of_device_id imx8mp_pinctrl_of_match[] = {
 
 static int imx8mp_pinctrl_probe(struct platform_device *pdev)
 {
-	const struct of_device_id *match;
-	struct imx_pinctrl_soc_info *pinctrl_info;
-
-	match = of_match_device(imx8mp_pinctrl_of_match, &pdev->dev);
-
-	if (!match)
-		return -ENODEV;
-
-	pinctrl_info = (struct imx_pinctrl_soc_info *) match->data;
-
-	return imx_pinctrl_probe(pdev, pinctrl_info);
+	return imx_pinctrl_probe(pdev, &imx8mp_pinctrl_info);
 }
 
 static struct platform_driver imx8mp_pinctrl_driver = {
