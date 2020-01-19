d12394877011f08a24dec2b8fad5a479f3d6eecf
Sandor Yu <Sandor.yu@nxp.com>
2020-01-19 10:23:40 +0800

    MLK-23250-05: irqchip: imx-irqsteer: Add reset function
    Add reset function for imx-irqsteer.
    
    Signed-off-by: Sandor Yu <Sandor.yu@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/irqchip/irq-imx-irqsteer.c
+++ drivers/irqchip/irq-imx-irqsteer.c
@@ -14,6 +14,7 @@
 #include <linux/of_platform.h>
 #include <linux/spinlock.h>
 #include <linux/pm_domain.h>
+#include <linux/reset.h>
 
 #define CTRL_STRIDE_OFF(_t, _r)	(_t * 4 * _r)
 #define CHANCTRL		0x0
@@ -213,6 +214,10 @@ static int imx_irqsteer_probe(struct platform_device *pdev)
 
 	ret = imx_irqsteer_attach_pd(data);
 	if (ret < 0 && ret == -EPROBE_DEFER)
+		return ret;
+
+	ret = device_reset(&pdev->dev);
+	if (ret == -EPROBE_DEFER)
 		return ret;
 
 	raw_spin_lock_init(&data->lock);
