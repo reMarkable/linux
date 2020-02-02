52f316af45c2b1bc10d2b181d8d52cb5c30055c8
Teo Hall <teo.hall@nxp.com>
2020-02-02 16:18:34 -0600

    MLK-23273-3 soc: imx: add imx8dxl soc id
    Add soc id for imx8dxl soc.
    
    Signed-off-by: Teo Hall <teo.hall@nxp.com>
    Reviewed-by: Anson Huang <Anson.Huang@nxp.com>

--- drivers/soc/imx/soc-imx-scu.c
+++ drivers/soc/imx/soc-imx-scu.c
@@ -121,6 +121,8 @@ static int imx_scu_soc_probe(struct platform_device *pdev)
 		TKT340553_SW_WORKAROUND = true;
 	} else if (of_machine_is_compatible("fsl,imx8qxp"))
 		soc_dev_attr->soc_id = "i.MX8QXP";
+	else if (of_machine_is_compatible("fsl, imx8dxl"))
+		soc_dev_attr->soc_id = "i.MX8DXL";
 
 	/* format revision value passed from SCU firmware */
 	val = (id >> 5) & 0xf;
