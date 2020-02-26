d364df4b4bb54eb07679d840e381e35764852fd1
Fugang Duan <fugang.duan@nxp.com>
2020-02-26 16:56:28 +0800

    MGS-5511 net: stmmac: start phylink instance before stmmac_hw_setup()
    Start phylink instance and resume back the PHY to supply
    RX clock to MAC before MAC layer initialization by calling
    .stmmac_hw_setup(), since DMA reset depends on the RX clock,
    otherwise DMA reset cost maximum timeout value then finally
    timeout.
    
    Reviewed-by: Richard Zhu <hongxing.zhu@nxp.com>
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -5030,6 +5030,12 @@ int stmmac_resume(struct device *dev)
 
 	netif_device_attach(ndev);
 
+	if (!device_may_wakeup(priv->device)) {
+		rtnl_lock();
+		phylink_start(priv->phylink);
+		rtnl_unlock();
+	}
+
 	mutex_lock(&priv->lock);
 
 	stmmac_reset_queues_param(priv);
@@ -5045,12 +5051,6 @@ int stmmac_resume(struct device *dev)
 	stmmac_start_all_queues(priv);
 
 	mutex_unlock(&priv->lock);
-
-	if (!device_may_wakeup(priv->device)) {
-		rtnl_lock();
-		phylink_start(priv->phylink);
-		rtnl_unlock();
-	}
 
 	phylink_mac_change(priv->phylink, true);
 
