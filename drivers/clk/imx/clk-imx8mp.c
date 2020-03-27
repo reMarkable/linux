e22d9b751b9e4a77e178a1940b24634c47233159
Jacky Bai <ping.bai@nxp.com>
2020-03-27 16:34:47 +0800

    MLK-23696-01 clk: imx: Correct the memrepair clock on imx8mp
    The root clock slice at offset 0xbf80 should be memrepair
    clock, so correct it. And this clock should be always on
    to make sure the memory repair function can works well.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by:  Anson Huang <Anson.Huang@nxp.com>

--- drivers/clk/imx/clk-imx8mp.c
+++ drivers/clk/imx/clk-imx8mp.c
@@ -460,7 +460,7 @@ static const char *imx8mp_media_ldb_sels[] = {"osc_24m", "sys_pll2_333m", "sys_p
 							"sys_pll1_800m", "sys_pll2_1000m", "clk_ext2",
 							"audio_pll2_out", "video_pll1_out", };
 
-static const char *imx8mp_media_mipi_csi2_esc_sels[] = {"osc_24m", "sys_pll2_100m", "sys_pll1_80m",
+static const char *imx8mp_memrepair_sels[] = {"osc_24m", "sys_pll2_100m", "sys_pll1_80m",
 							"sys_pll1_800m", "sys_pll2_1000m", "sys_pll3_out",
 							"clk_ext3", "audio_pll2_out", };
 
@@ -753,7 +753,7 @@ static int imx8mp_clocks_probe(struct platform_device *pdev)
 	clks[IMX8MP_CLK_MEDIA_DISP1_PIX] = imx8m_clk_composite("media_disp1_pix", imx8mp_media_disp1_pix_sels, base + 0xbe00);
 	clks[IMX8MP_CLK_MEDIA_CAM2_PIX] = imx8m_clk_composite("media_cam2_pix", imx8mp_media_cam2_pix_sels, base + 0xbe80);
 	clks[IMX8MP_CLK_MEDIA_LDB] = imx8m_clk_composite("media_ldb", imx8mp_media_ldb_sels, base + 0xbf00);
-	clks[IMX8MP_CLK_MEDIA_MIPI_CSI2_ESC] = imx8m_clk_composite("media_mipi_csi2_esc", imx8mp_media_mipi_csi2_esc_sels, base + 0xbf80);
+	clks[IMX8MP_CLK_MEMREPAIR] = imx8m_clk_composite_critical("mem_repair", imx8mp_memrepair_sels, base + 0xbf80);
 	clks[IMX8MP_CLK_PCIE2_CTRL] = imx8m_clk_composite("pcie2_ctrl", imx8mp_pcie2_ctrl_sels, base + 0xc000);
 	clks[IMX8MP_CLK_PCIE2_PHY] = imx8m_clk_composite("pcie2_phy", imx8mp_pcie2_phy_sels, base + 0xc080);
 	clks[IMX8MP_CLK_MEDIA_MIPI_TEST_BYTE] = imx8m_clk_composite("media_mipi_test_byte", imx8mp_media_mipi_test_byte_sels, base + 0xc100);
