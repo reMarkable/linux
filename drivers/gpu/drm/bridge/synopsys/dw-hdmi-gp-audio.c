40a1d40b498fb9bf4666615776dcbc5fcd3e05e6
Shengjiu Wang <shengjiu.wang@nxp.com>
2020-05-20 12:52:54 +0800

    MLK-24076-2: drm: imx: hdmi: Add copyright
    Add copyright
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi-gp-audio.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi-gp-audio.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
+// Copyright 2020 NXP
 /*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
