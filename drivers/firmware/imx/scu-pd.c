704b9cbd87dfd0f24e735b6401ece6b93e2d12a0
Alice Guo <alice.guo@nxp.com>
2020-05-20 22:22:45 +0800

    MLK-24064-1: firmware: imx: scu-pd: add power domains for lpuart
    Add power domains for lpuart in ARM Cortex-M4 (CM4) Subsystem.
    
    Signed-off-by: Alice Guo <alice.guo@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- drivers/firmware/imx/scu-pd.c
+++ drivers/firmware/imx/scu-pd.c
@@ -226,12 +226,14 @@ static const struct imx_sc_pd_range imx8qxp_scu_pd_ranges[] = {
 	{ "cm40_intmux", IMX_SC_R_M4_0_INTMUX, 1, false, 0 },
 	{ "cm40_pid", IMX_SC_R_M4_0_PID0, 5, true, 0},
 	{ "cm40_mu1a", IMX_SC_R_M4_0_MU_1A, 1, false, 0},
+	{ "cm40_lpuart", IMX_SC_R_M4_0_UART, 1, false, 0},
 
 	/* CM41 SS */
 	{ "cm41_i2c", IMX_SC_R_M4_1_I2C, 1, false, 0 },
 	{ "cm41_intmux", IMX_SC_R_M4_1_INTMUX, 1, false, 0 },
 	{ "cm41_pid", IMX_SC_R_M4_1_PID0, 5, true, 0},
 	{ "cm41_mu1a", IMX_SC_R_M4_1_MU_1A, 1, false, 0},
+	{ "cm41_lpuart", IMX_SC_R_M4_1_UART, 1, false, 0},
 
 	/* IMAGE SS */
 	{ "img-pdma", IMX_SC_R_ISI_CH0, 8, true, 0 },
