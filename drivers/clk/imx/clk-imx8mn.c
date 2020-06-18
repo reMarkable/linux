2d64e8e2dba409491349cefaa55a2ac9ecceece1
Anson Huang <Anson.Huang@nxp.com>
2020-06-18 16:22:00 +0800

    MLK-24337-5 clk: imx8mn: Support module build
    Add module author, description and license to support module build.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

--- drivers/clk/imx/clk-imx8mn.c
+++ drivers/clk/imx/clk-imx8mn.c
@@ -685,3 +685,6 @@ static struct platform_driver imx8mn_clk_driver = {
 	},
 };
 module_platform_driver(imx8mn_clk_driver);
+MODULE_AUTHOR("Anson Huang <Anson.Huang@nxp.com>");
+MODULE_DESCRIPTION("NXP i.MX8MN clock driver");
+MODULE_LICENSE("GPL v2");
