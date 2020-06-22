45483d0ced8e415cf6ba08e92770db06f318de01
zhang sanshan <pete.zhang@nxp.com>
2020-06-22 10:50:19 +0800

    MA-17422-1 genirq: Export irq_chip_pm_get and irq_chip_pm_put
    Add EXPORT_SYMBOL_GPL entries for irq_chip_pm_get() and
    and irq_chip_pm_put() so that we can allow drivers
    like the gpio-mxc driver to be loadable as a module.
    
    Signed-off-by: zhang sanshan <pete.zhang@nxp.com>
    Reviewed-by: Fugang Duan <fugang.duan@nxp.com>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -1545,6 +1545,7 @@ int irq_chip_pm_get(struct irq_data *data)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(irq_chip_pm_get);
 
 /**
  * irq_chip_pm_put - Disable power for an IRQ chip
@@ -1563,3 +1564,4 @@ int irq_chip_pm_put(struct irq_data *data)
 
 	return (retval < 0) ? retval : 0;
 }
+EXPORT_SYMBOL_GPL(irq_chip_pm_put);
