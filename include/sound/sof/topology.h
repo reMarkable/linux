f24c043d4a641e103dc752d314ca5a9ec78fbab4
Daniel Baluta <daniel.baluta@nxp.com>
2020-08-17 18:48:39 +0300

    MLK-24255-2 ASoC: SOF: Introduce POST_PROCESS component type
    POST_PROCESS component takes care of processing algorithms. For now,
    we intend to use this component for decoding/encoding library algorithms
    from Cadence.
    
    Current implementation assumes that we will still use ALSA PCM
    interface. The heavy part is done on firmware side with the help
    of topology files.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- include/sound/sof/topology.h
+++ include/sound/sof/topology.h
@@ -38,6 +38,8 @@ enum sof_comp_type {
 	SOF_COMP_DEMUX,
 	SOF_COMP_ASRC,
 	SOF_COMP_DCBLOCK,
+	SOF_COMP_PP,
+
 	/* keep FILEREAD/FILEWRITE as the last ones */
 	SOF_COMP_FILEREAD = 10000,	/**< host test based file IO */
 	SOF_COMP_FILEWRITE = 10001,	/**< host test based file IO */
@@ -181,6 +183,7 @@ enum sof_ipc_process_type {
 	SOF_PROCESS_MUX,
 	SOF_PROCESS_DEMUX,
 	SOF_PROCESS_DCBLOCK,
+	SOF_PROCESS_PP, /** < Post process */
 };
 
 /* generic "effect", "codec" or proprietary processing component */
