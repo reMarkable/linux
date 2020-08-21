cd078fac33b60e753886ded2e821ef765a137110
Iuliana Prodan <iuliana.prodan@nxp.com>
2020-08-21 17:30:58 +0300

    MLK-24517-1 crypto: caam - removed unnecessary validation of black key for blob decapsulation
    The address of an array is never NULL, so the comparison of
    black key array always evaluate the same way.
    Remove unnecessary check.
    
    Reported-by: Coverity 10893156
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>
    Reviewed-by: Franck LENORMAND <franck.lenormand@nxp.com>

--- drivers/crypto/caam/caamkeyblob.c
+++ drivers/crypto/caam/caamkeyblob.c
@@ -531,7 +531,7 @@ int caam_blob_decap(struct device *dev, struct keyblob_info *info)
 	trusted_key = (info->type >> TAG_OBJ_TK_OFFSET) & 0x1;
 
 	/* Validate input data*/
-	if (!info->black_key || !info->key_mod || !blob)
+	if (!info->key_mod || !blob)
 		return -EINVAL;
 
 	dev_dbg(dev, "%s input: [blob: %p (%zu), mem_type: %x, color: %x",
