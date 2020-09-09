271cdc141d35284e18a8b3d350867c0732a6814f
Franck LENORMAND <franck.lenormand@nxp.com>
2020-09-09 09:21:29 +0200

    MLK-24759: fw: imx: scu: rm: Fix name of some structures
    The structures used by imx_sc_rm_find_memreg() and
    imx_sc_rm_get_resource_owner() had a "misc" in their name
    but they are for the RM API.
    
    This patch replaces the "misc" part by "rm".
    
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>
    Reviewed-by: Iuliana Prodan <iuliana.prodan@nxp.com>
    Reviewed-by: Horia Geantă <horia.geanta@nxp.com>

--- drivers/firmware/imx/rm.c
+++ drivers/firmware/imx/rm.c
@@ -40,7 +40,7 @@ bool imx_sc_rm_is_resource_owned(struct imx_sc_ipc *ipc, u16 resource)
 }
 EXPORT_SYMBOL(imx_sc_rm_is_resource_owned);
 
-struct imx_sc_msg_misc_find_memreg {
+struct imx_sc_msg_rm_find_memreg {
 	struct imx_sc_rpc_msg hdr;
 	union {
 		struct {
@@ -58,7 +58,7 @@ struct imx_sc_msg_misc_find_memreg {
 int imx_sc_rm_find_memreg(struct imx_sc_ipc *ipc, u8 *mr, u64 addr_start,
 			  u64 addr_end)
 {
-	struct imx_sc_msg_misc_find_memreg msg;
+	struct imx_sc_msg_rm_find_memreg msg;
 	struct imx_sc_rpc_msg *hdr = &msg.hdr;
 	int ret;
 
@@ -83,7 +83,7 @@ int imx_sc_rm_find_memreg(struct imx_sc_ipc *ipc, u8 *mr, u64 addr_start,
 }
 EXPORT_SYMBOL(imx_sc_rm_find_memreg);
 
-struct imx_sc_msg_misc_get_resource_owner {
+struct imx_sc_msg_rm_get_resource_owner {
 	struct imx_sc_rpc_msg hdr;
 	union {
 		struct {
@@ -97,7 +97,7 @@ struct imx_sc_msg_misc_get_resource_owner {
 
 int imx_sc_rm_get_resource_owner(struct imx_sc_ipc *ipc, u16 resource, u8 *pt)
 {
-	struct imx_sc_msg_misc_get_resource_owner msg;
+	struct imx_sc_msg_rm_get_resource_owner msg;
 	struct imx_sc_rpc_msg *hdr = &msg.hdr;
 	int ret;
 
