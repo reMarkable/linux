e69448bf61fa299985a02e752cf7a2200c6d682b
Lars Ivar Miljeteig <lars.ivar.miljeteig@remarkable.no>
2020-09-23 13:31:06 +0200

    rm-otgcontrol: Add licensing info

--- drivers/misc/rm-otgcontrol/otgcontrol_dr_mode.c
+++ drivers/misc/rm-otgcontrol/otgcontrol_dr_mode.c
@@ -1,3 +1,20 @@
+/*
+ * reMarkable OTG Control
+ *
+ * Copyright (C) 2019 reMarkable AS - http://www.remarkable.com/
+ *
+ * Author: Steinar Bakkemo <steinar.bakkemo@remarkable.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include "otgcontrol_dr_mode.h"
 
 #include <linux/kernel.h>
