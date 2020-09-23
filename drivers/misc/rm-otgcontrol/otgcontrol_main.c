627b4c07b76686d1c4a8ff22d8bced938d248017
Lars Ivar Miljeteig <lars.ivar.miljeteig@remarkable.no>
2020-09-23 13:31:06 +0200

    rm-otgcontrol: Add licensing info

--- drivers/misc/rm-otgcontrol/otgcontrol_main.c
+++ drivers/misc/rm-otgcontrol/otgcontrol_main.c
@@ -1,11 +1,18 @@
 /*
- * Sample kobject implementation
+ * reMarkable OTG Control
  *
- * Copyright (C) 2004-2007 Greg Kroah-Hartman <greg@kroah.com>
- * Copyright (C) 2007 Novell Inc.
+ * Copyright (C) 2019 reMarkable AS - http://www.remarkable.com/
  *
- * Released under the GPL version 2 only.
+ * Author: Steinar Bakkemo <steinar.bakkemo@remarkable.com>
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #include "otgcontrol.h"
