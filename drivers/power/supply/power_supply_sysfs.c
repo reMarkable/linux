d0422f8316c62b84fbc902d1e2808c61283b74d3
Steinar Bakkemo <steinar.bakkemo@remarkable.no>
2020-10-06 12:36:05 +0200

    power-supply: add POWER_SUPPLY_PROP_STATUS_EX property with enumarated values

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -81,6 +81,10 @@ static const char * const power_supply_charger_mode_text[] = {
 	"Charger", "OTG Supply", "Off"
 };
 
+static const char * const power_supply_status_ex_text[] = {
+	"Charger not connected", "POGO connected", "USB-C connected", "POGO/USB-C connected"
+};
+
 static ssize_t power_supply_show_usb_type(struct device *dev,
 					  enum power_supply_usb_type *usb_types,
 					  ssize_t num_usb_types,
@@ -179,6 +183,10 @@ static ssize_t power_supply_show_property(struct device *dev,
 		ret = sprintf(buf, "%s\n",
 			      power_supply_charger_mode_text[value.intval]);
 		break;
+	case POWER_SUPPLY_PROP_STATUS_EX:
+		return sprintf(buf, "%s\n",
+			       power_supply_status_ex_text[value.intval]);
+		break;
 	case POWER_SUPPLY_PROP_MODEL_NAME ... POWER_SUPPLY_PROP_SERIAL_NUMBER:
 		ret = sprintf(buf, "%s\n", value.strval);
 		break;
@@ -219,7 +227,9 @@ static ssize_t power_supply_store_property(struct device *dev,
 	case POWER_SUPPLY_PROP_CHARGER_MODE:
 		ret = sysfs_match_string(power_supply_charger_mode_text, buf);
 		break;
-
+	case POWER_SUPPLY_PROP_STATUS_EX:
+		ret = sysfs_match_string(power_supply_status_ex_text, buf);
+		break;
 	default:
 		ret = -EINVAL;
 	}
@@ -323,8 +333,15 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(manufacturer),
 	POWER_SUPPLY_ATTR(serial_number),
 
-	/* MAX77818-charger specific property to switch between OTG Supply (host mode) and charging (device mode) */
+	/* MAX77818-charger specific property to switch between OTG Supply (host mode)
+	 * and charging (device mode)
+	 */
 	POWER_SUPPLY_ATTR(charger_mode),
+
+	/* MAX77818-charger specific property to get extended charger status indicating
+	 * which of the two charger inputs are connected
+	 */
+	POWER_SUPPLY_ATTR(status_ex),
 };
 
 static struct attribute *
