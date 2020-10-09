5bb09de3044acf91e6f8c712c026d5a4bc53b563
Jason Liu <jason.hui.liu@nxp.com>
2020-10-09 11:15:34 +0800

    MLK-24874-1: include: uapi: fmd: fix the UAPI_HEADER_TEST/install failure
    Enable CONFIG_UAPI_HEADER_TEST or make headers_install result in build issues:
    
    include/uapi/linux/fmd/Peripherals/fm_port_ioctls.h: leak CONFIG_COMPAT to user-space
    include/uapi/linux/fmd/Peripherals/fm_pcd_ioctls.h: leak CONFIG_COMPAT to user-space
    include/uapi/linux/fmd/Peripherals/fm_ioctls.h: leak CONFIG_COMPAT to user-space
    
    The comments from DN fix of the fmd related failure is to add to the skip-list due to the
    historial reasons from DN(Layerscape) drivers - to avoid break the legacy users
    
    Signed-off-by: Jason Liu <jason.hui.liu@nxp.com>
    (cherry picked from commit 569d6d3cc2a664de66ee980bd611687f4d785b4e)

--- scripts/headers_install.sh
+++ scripts/headers_install.sh
@@ -94,6 +94,9 @@ include/uapi/linux/eventpoll.h:CONFIG_PM_SLEEP
 include/uapi/linux/hw_breakpoint.h:CONFIG_HAVE_MIXED_BREAKPOINTS_REGS
 include/uapi/linux/pktcdvd.h:CONFIG_CDROM_PKTCDVD_WCACHE
 include/uapi/linux/raw.h:CONFIG_MAX_RAW_DEVS
+include/uapi/linux/fmd/Peripherals/fm_port_ioctls.h:CONFIG_COMPAT
+include/uapi/linux/fmd/Peripherals/fm_pcd_ioctls.h:CONFIG_COMPAT
+include/uapi/linux/fmd/Peripherals/fm_ioctls.h:CONFIG_COMPAT
 "
 
 for c in $configs
