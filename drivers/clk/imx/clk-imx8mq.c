4a7df595092e9ab9bb78696c0306c533fbfdde0c
Jacky Bai <ping.bai@nxp.com>
2020-10-27 14:58:32 +0800

    MLK-24922 clk: imx: Add CLK_GET_RATE_NOCACHE flag for dram pll on imx8mq
    As the DRAM PLL frequency can be changed during busfreq transition by
    TF-A on i.MX8MQ, add 'CLK_GET_RATE_NOCACHE' flag for dram pll to make
    sure linux kernel can get the correct rate of this pll.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Anson Huang <anson.huang@nxp.com>

--- drivers/clk/imx/clk-imx8mq.c
+++ drivers/clk/imx/clk-imx8mq.c
@@ -379,7 +379,7 @@ static int imx8mq_clocks_probe(struct platform_device *pdev)
 	clks[IMX8MQ_SYS1_PLL_OUT] = imx_clk_sccg_pll("sys1_pll_out", sys1_pll_out_sels, ARRAY_SIZE(sys1_pll_out_sels), 0, 0, 0, base + 0x30, CLK_IS_CRITICAL);
 	clks[IMX8MQ_SYS2_PLL_OUT] = imx_clk_sccg_pll("sys2_pll_out", sys2_pll_out_sels, ARRAY_SIZE(sys2_pll_out_sels), 0, 0, 1, base + 0x3c, CLK_IS_CRITICAL);
 	clks[IMX8MQ_SYS3_PLL_OUT] = imx_clk_sccg_pll("sys3_pll_out", sys3_pll_out_sels, ARRAY_SIZE(sys3_pll_out_sels), 0, 0, 1, base + 0x48, CLK_IS_CRITICAL);
-	clks[IMX8MQ_DRAM_PLL_OUT] = imx_clk_sccg_pll("dram_pll_out", dram_pll_out_sels, ARRAY_SIZE(dram_pll_out_sels), 0, 0, 0, base + 0x60, CLK_IS_CRITICAL);
+	clks[IMX8MQ_DRAM_PLL_OUT] = imx_clk_sccg_pll("dram_pll_out", dram_pll_out_sels, ARRAY_SIZE(dram_pll_out_sels), 0, 0, 0, base + 0x60, CLK_IS_CRITICAL | CLK_GET_RATE_NOCACHE);
 	clks[IMX8MQ_VIDEO2_PLL_OUT] = imx_clk_sccg_pll("video2_pll_out", video2_pll_out_sels, ARRAY_SIZE(video2_pll_out_sels), 0, 0, 0, base + 0x54, 0);
 
 	/* SYS PLL fixed output */
